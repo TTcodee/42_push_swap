014a552e01317d542c9cf30085eaced30c648232
mos_at_laptop <mospakawat@gmail.com>
2022-12-11 22:52:47 +0700

    initial

---
+++ stack.c
@@ -0,0 +1,32 @@
+#include "stack.h"
+
+int	push(t_stack *self, void *data)
+{
+	self->size = self->stack->len + 1;
+	return (self->stack->addlast(self->stack, data));
+}
+
+void	*pop(t_stack *self)
+{
+	void	*tmp;
+	tmp = self->stack->last->data;
+	self->stack->rmlast(self->stack, NULL );
+	self->size = self->stack->len;
+	return (tmp);
+}
+
+t_stack	*stack_init()
+{
+	t_stack	*ret;
+
+	ret = malloc(sizeof(t_stack));
+	if (!ret)
+		return (NULL);
+	ret->stack = dlist_init();
+	if (!ret->stack)
+		return (NULL);
+	ret->size = 0;
+	ret->push = push;
+	ret->pop = pop;
+	return (ret);
+}
