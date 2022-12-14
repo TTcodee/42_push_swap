a8bd1e444dbb7cd0b439f363dfa8e6ae184f32b9
mos_at_laptop <mospakawat@gmail.com>
2022-12-14 15:37:59 +0700

    Add push and pop

---
+++ srcs/container/dlist.h
@@ -0,0 +1,32 @@
+#ifndef DLIST_H
+# define DLIST_H
+# include <stdlib.h>
+
+typedef struct s_dnode t_dnode;
+typedef struct s_dlist t_dlist;
+
+struct s_dnode
+{
+	t_dnode	*next;
+	t_dnode	*back;
+	void	*data;
+};
+
+struct s_dlist
+{
+	t_dnode		*start;
+	t_dnode		*last;
+	unsigned int	len;
+	int		(*addfirst)(t_dlist *self, void *);
+	int		(*addlast)(t_dlist *self, void *);
+	void		(*rmfirst)(t_dlist *self, void (*del)(void *));
+	void		(*rmlast)(t_dlist *self, void (*del)(void *));
+};
+
+int	addfirst(t_dlist *self, void *data);
+int	addlast(t_dlist *self, void *data);
+void	rmfirst(t_dlist *self, void (*del)(void *));
+void	rmlast(t_dlist *self, void (*del)(void *));
+t_dlist	*dlist_init(void);
+
+#endif
